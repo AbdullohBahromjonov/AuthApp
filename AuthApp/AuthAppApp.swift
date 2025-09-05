e02aa372870b4f72f92359e33c7524d3969cd74f
Abdulloh <abdullohbahromjonv@gmail.com>
2025-09-05 17:10:43 +0500

    Initial Commit

---
+++ AuthApp/AuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppApp.swift
+//  AuthApp
+//
+//  Created by Abdulloh Bahromjonov on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
